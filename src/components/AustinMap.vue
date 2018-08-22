9983b761c21a3636f52cd378c6cbdb23e8c0d573
Marc Majcher <majcher@gmail.com>
2018-08-22 11:56:26 -0500

    lint

--- src/components/AustinMap.vue
+++ src/components/AustinMap.vue
@@ -7,25 +7,27 @@
 
 <style>
 #austinmap {
-  height: 400px;
+  height: 500px;
 }
 </style>
 
 <script>
+/* globals L */
+
 export default {
   mounted: () => {
-    const austinMap = L.map("austinmap").setView([30.27, -97.74], 13);
+    const austinMap = L.map('austinmap').setView([30.27, -97.74], 13);
     L.tileLayer(
-      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+      'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
       {
         attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        id: "mapbox.streets",
+        id: 'mapbox.streets',
         accessToken:
-          "pk.eyJ1IjoibWFqY2hlciIsImEiOiJjamw0ZGY5NG4yYXhvM3Fxamd0YXhjbXA5In0.RJaLDHr064DigHF4bXGYoA"
-      }
+          'pk.eyJ1IjoibWFqY2hlciIsImEiOiJjamw0ZGY5NG4yYXhvM3Fxamd0YXhjbXA5In0.RJaLDHr064DigHF4bXGYoA',
+      },
     ).addTo(austinMap);
-  }
-}
+  },
+};
 </script>
--- src/components/MapGame.vue
+++ src/components/MapGame.vue
@@ -1,32 +1,34 @@
 <template>
   <div class="map">
-    <h1>Map Game Demo</h1>
+    <h1>Map Demo</h1>
     <div id="mapgameid"></div>
   </div>
 </template>
 
 <style>
 #mapgameid {
-  height: 400px;
+  height: 500px;
 }
 </style>
 
 <script>
+/* globals L */
+
 export default {
   mounted: () => {
-    const mymap = L.map("mapgameid").setView([40.7218, -74], 13);
+    const mymap = L.map('mapgameid').setView([40.7218, -74], 13);
 
     L.tileLayer(
-      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+      'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
       {
         attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        id: "mapbox.streets",
+        id: 'mapbox.streets',
         accessToken:
-          "pk.eyJ1IjoibWFqY2hlciIsImEiOiJjamw0ZGY5NG4yYXhvM3Fxamd0YXhjbXA5In0.RJaLDHr064DigHF4bXGYoA"
-      }
+          'pk.eyJ1IjoibWFqY2hlciIsImEiOiJjamw0ZGY5NG4yYXhvM3Fxamd0YXhjbXA5In0.RJaLDHr064DigHF4bXGYoA',
+      },
     ).addTo(mymap);
-  }
-}
+  },
+};
 </script>
