8c4384aeefb77a50d772b84b06c15160a412bd45
Marc Majcher <majcher@gmail.com>
2018-08-29 22:45:24 -0500

    jm component

---
+++ src/components/JerrysMap.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="map">
+    <h1>Map Demo</h1>
+    <div id="jerrysmap"></div>
+  </div>
+</template>
+
+<style>
+#mapgameid {
+  height: 800px;
+}
+</style>
+
+<script>
+/* globals L */
+
+export default {
+  mounted: () => {
+    const mymap = L.map('jerrysmap').setView([68, -80], 3);
+    L.tileLayer(
+      'img/{id}/tile_{z}_{x}_{y}.png',
+      {
+      //  tileSize: L.point(240, 300), 
+        attribution:
+          'Jerry Gretzinger',
+        maxZoom: 7,
+        minZoom: 2,
+        id: 'jerrysmap',
+      },
+    ).addTo(mymap);
+  },
+};
+</script>
